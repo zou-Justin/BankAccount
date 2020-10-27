35590f958b6f910c53f2d6a48fca33b6049610e0
Justin Dylan <justin82049@gmail.com>
2020-10-26 22:36:13 -0400

    added deposit method

--- BankAccount.java
+++ BankAccount.java
@@ -16,6 +16,13 @@ public class BankAccount{
   public void setPassword(String newPass){
     password = newPass;
   }
+  public boolean deposit(double amount){
+    if (amount > 0){
+      balance += amount;
+      return true;
+    }
+    return false;
+  }
   public String getInfo(){
     return (password + " " + accountID + " " + balance);
   }
--- BankAccountTester.java
+++ BankAccountTester.java
@@ -2,6 +2,7 @@ public class BankAccountTester{
   public static void main(String[]args){
     BankAccount b1 = new BankAccount("hello123",12);
     b1.setPassword("noone");
+    b1.deposit(3);
     System.out.println(b1.getInfo());
   }
 
