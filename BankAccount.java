c742f800107a5e028210587d355c5dea5beff354
Justin Dylan <justin82049@gmail.com>
2020-10-26 22:47:33 -0400

    added withdraw method

--- BankAccount.java
+++ BankAccount.java
@@ -23,6 +23,13 @@ public class BankAccount{
     }
     return false;
   }
+  public boolean withdraw(double amount){
+    if (amount > 0 || amount > balance){
+      balance = balance - amount;
+      return true;
+    }
+    return false;
+  }
   public String getInfo(){
     return (password + " " + accountID + " " + balance);
   }
--- BankAccountTester.java
+++ BankAccountTester.java
@@ -3,6 +3,7 @@ public class BankAccountTester{
     BankAccount b1 = new BankAccount("hello123",12);
     b1.setPassword("noone");
     b1.deposit(3);
+    b1.withdraw(2);
     System.out.println(b1.getInfo());
   }
 
